fc69d87602a1c4c5de1cd09e99415f4d3da4e003
Thomas Wolfe <wolfe2tr@dukes.jmu.edu>
2019-10-27 22:02:55 -0400

    Test

--- skyroom/App_Code/Messages.cs
+++ skyroom/App_Code/Messages.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-/// <summary>
-/// Summary description for PropertyRoom
-/// </summary>
 public class Messages
 {
     private int messageID;
