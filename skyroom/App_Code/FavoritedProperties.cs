877aedcdf0a13faa1a37611515afb9e892175066
Thomas Wolfe <wolfe2tr@dukes.jmu.edu>
2019-10-27 21:57:38 -0400

    Messages class test

---
+++ skyroom/App_Code/FavoritedProperties.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for PropertyRoom
+/// </summary>
+public class FavoritedProperties
+{
+    public FavoritedProperties()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
---
+++ skyroom/App_Code/Messages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for PropertyRoom
+/// </summary>
+public class Messages
+{
+    private int messageID;
+    private int favPropID;
+    private int favTenantID;
+    private string messageContent;
+    private DateTime dateTime; 
+
+    public Messages()
+    {
+
+
+
+    }
+}
